a889bc9bee60f1f176176f2e3fd83b66f53fd958
Vaibhav Kambli <vaibhavk2608@gmail.com>
2021-01-10 01:09:02 -0500

    Updated meta title

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Vaibhav'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Vaibhav\'s Blog'
 
 export default function Layout({ children, home }) {
   return (
